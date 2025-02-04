6a80f74c6898642953cfcb2496190cc2021fb398
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2025-02-04 16:30:59 +0700

    7

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Đảm bảo thư mục build là 'dist', mặc định của Vercel
+    outDir: "dist",
     rollupOptions: {
       output: {
-        // Đảm bảo tên file sau khi build không bị trùng
         entryFileNames: "assets/[name].[hash].js",
         chunkFileNames: "assets/[name].[hash].js",
         assetFileNames: "assets/[name].[hash].[ext]",
       },
     },
-  },
-  optimizeDeps: {
-    include: ["@babel/runtime"],
+    minify: "esbuild",
+    sourcemap: true,
+    outDir: "dist",
   },
   server: {
-    open: true, // Tự động mở trình duyệt khi chạy dev server
+    open: true,
   },
 });

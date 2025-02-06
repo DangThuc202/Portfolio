99bc567ae2d0a10d571857567c6a3a0b7e3f4547
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2025-02-06 08:44:41 +0700

    14

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    open: true, // Tự động mở trình duyệt khi chạy dev server
-    port: 3000, // Đảm bảo chạy trên port 3000 (hoặc đổi nếu cần)
-    host: true, // Để có thể truy cập từ mạng LAN nếu cần
-  },
+  base: "/Portfolio",
 });

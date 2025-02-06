87a6c3758ceee9af39f31dc1b486fb8bc80ed931
ThucDang <85912398+DangThuc202@users.noreply.github.com>
2025-02-06 09:24:04 +0700

    20

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/", // Chỉ dùng base khi build
+  base: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/", // Đảm bảo base đúng
+  optimizeDeps: {
+    include: ["@babel/runtime/helpers/createSuper"],
+  },
 });
